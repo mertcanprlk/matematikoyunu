c1ec2eab3f8e5911dd591e0e98c7ba7f220ede32
mertcanprlk <mert.166169@gmail.com>
2022-08-25 16:48:34 +0300

    asd

---
+++ Assets/Buton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buton : MonoBehaviour
+{
+    public Text SahnedekiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButonIslemler(){
+
+        SahnedekiText.text = "Merhaba UI";
+
+    }
+}
